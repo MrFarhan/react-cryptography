f9ad2a9a20f9bb766bf38ef5061a4d5fd650d7b7
MrFarhan <muhammadfarhan012345@gmail.com>
2023-12-27 05:47:20 +0500

    Updated UI and added Dropdown to select DES, TrippleDes and AES for encryption and decryption.
    Changes also deployed

--- src/App.js
+++ src/App.js
@@ -1,110 +1,130 @@
 import CryptoJS from "crypto-js";
-import { useState } from "react";
+import { useState, useCallback } from "react";
+import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
   const [screen, setScreen] = useState("encrypt");
-
-  const [encrptedData, setEncrptedData] = useState("");
-  const [decrptedData, setDecrptedData] = useState("");
-  const [error, setError] = useState(null); // New state for error handling
+  const [algorithm, setAlgorithm] = useState("AES");
+  const [result, setResult] = useState({ data: "", error: null });
 
   const secretPass = "XkhZG4fW2t2W";
 
-  const encryptData = () => {
-    const data = CryptoJS.AES.encrypt(
-      JSON.stringify(text),
-      secretPass
-    ).toString();
+  const resetStates = () => {
+    setText("");
+    setResult({ data: "", error: null });
+  };
 
-    setEncrptedData(data);
-    setError(null);
+  const encryptData = () => {
+    try {
+
+      let data;
+      switch (algorithm) {
+        case "AES":
+          data = CryptoJS.AES.encrypt(JSON.stringify(text), secretPass).toString();
+          break;
+        case "TripleDES":
+          data = CryptoJS.TripleDES.encrypt(JSON.stringify(text), secretPass).toString();
+          break;
+        case "DES":
+          data = CryptoJS.DES.encrypt(JSON.stringify(text), secretPass).toString();
+          break;
+        default:
+          break;
+      }
+      setResult({ data, error: null });
+    } catch (e) {
+      setResult({ data: "", error: "Encryption failed. Please check your input." });
+    }
   };
 
   const decryptData = () => {
     try {
-      const bytes = CryptoJS.AES.decrypt(text, secretPass);
-      const data = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-      setDecrptedData(data);
-      setError(null);
+      let bytes, data;
+      switch (algorithm) {
+        case "AES":
+          bytes = CryptoJS.AES.decrypt(text, secretPass);
+          break;
+        case "TripleDES":
+          bytes = CryptoJS.TripleDES.decrypt(text, secretPass);
+          break;
+        case "DES":
+          bytes = CryptoJS.DES.decrypt(text, secretPass);
+          break;
+        default:
+          break;
+      }
+
+      data = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+      setResult({ data, error: null });
     } catch (e) {
-      setError("Decryption failed. Please check your decryption key.");
+      setResult({ data: "", error: "Decryption failed. Please check your decryption key." });
     }
   };
 
-  const switchScreen = (type) => {
-    setText("");
-    setEncrptedData("");
-    setDecrptedData("");
-    setError(null);
-    setScreen(type);
-  };
-
-  const handleClick = () => {
-    if (!text) return;
+  const handleButtonClick = useCallback(() => {
+    if (!text) return setResult({ data: "", error: "Please type something that you want to encrypt." });;
 
     if (screen === "encrypt") encryptData();
     else decryptData();
-  };
+  }, [text, screen, algorithm]);
+
+  const switchScreen = useCallback((type) => {
+    resetStates();
+    setScreen(type);
+  }, []);
 
   return (
     <div className="container">
-      <div>
+      <h1>Cryptography Tool</h1>
+      <div className="button-container">
         <button
-          className="btn btn-left"
-          style={{
-            backgroundColor: screen === "encrypt" ? "#5e35b1" : "#5e35b130",
-          }}
-          onClick={() => {
-            switchScreen("encrypt");
-          }}
+          className={screen === "encrypt" ? "active" : ""}
+          onClick={() => switchScreen("encrypt")}
         >
           Encrypt
         </button>
-
         <button
-          className="btn btn-right"
-          style={{
-            backgroundColor: screen === "decrypt" ? "#1e88e5" : "#1e88e530",
-          }}
-          onClick={() => {
-            switchScreen("decrypt");
-          }}
+          className={screen === "decrypt" ? "active" : ""}
+          onClick={() => switchScreen("decrypt")}
         >
           Decrypt
         </button>
       </div>
 
-      <div className="card">
-        <input
-          value={text}
-          onChange={({ target }) => {
-            setText(target.value);
-          }}
-          name="text"
-          type="text"
-          placeholder={
-            screen === "encrypt" ? "Enter Text" : "Enter Encrypted Data"
-          }
-        />
-
-        <button className="btn submit-btn" onClick={handleClick}>
-          {screen === "encrypt" ? "Encrypt" : "Decrypt"}
-        </button>
+      <div className="select-wrapper">
+        <select
+          value={algorithm}
+          onChange={({ target }) => setAlgorithm(target.value)}
+        >
+          <option value="AES">AES</option>
+          <option value="TripleDES">Triple DES</option>
+          <option value="DES">DES</option>
+        </select>
       </div>
 
-      {error ? (
-        <div className="error">
-          <p>{error}</p>
-        </div>
-      ) : null}
+      <div className="input-container">
+        <input
+          value={text}
+          onChange={({ target }) => setText(target.value)}
+          name="text"
+          type="text"
+          placeholder={screen === "encrypt" ? "Enter Text" : "Enter Encrypted Data"}
+        />
+      </div>
 
-      {encrptedData || decrptedData ? (
+      <button className="submit-btn" onClick={handleButtonClick}>
+        {screen === "encrypt" ? "Encrypt" : "Decrypt"}
+      </button>
+
+      {result.error && <div className="error">{result.error}</div>}
+
+      {result.data && (
         <div className="content">
           <label>{screen === "encrypt" ? "Encrypted" : "Decrypted"} Data</label>
-          <p>{screen === "encrypt" ? encrptedData : decrptedData}</p>
+          <p>{result.data}</p>
         </div>
-      ) : null}
+      )}
     </div>
   );
 }
